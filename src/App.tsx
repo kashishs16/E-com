98a33fbb67967ef8c2d60c77f85abf65797aab4c
Kashish <kashishsingh.mentem@gmail.com>
2024-01-12 14:28:29 +0530

    footer added

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import api from "components/Api";
 import Header from "./components/Header"
 import "./index.css";
 import Sliders from "components/Sliders";
+import Footer from "components/Footer";
 
-function App() {  
+function App() {
   const [data, setData] = useState<Array<TDetails>>([{
     id: 0,
     price: 0,
@@ -25,7 +26,7 @@ function App() {
   const [errmsg, setErrmsg] = useState("");
   const [searchedValue, setSearchedValue] = useState("");
   // console.log(input);
-  
+
   async function fetchApi() {
     try {
       const res = await api.get("/products");
@@ -57,7 +58,7 @@ function App() {
         arrObj.push(val);
       } else if (selectedCat === "all products") {
         arrObj.push(val);
-      } 
+      }
     })
     setFilteredData(arrObj);
   }, [selectedCat, searchedValue])
@@ -68,9 +69,10 @@ function App() {
         <Header catArr={catArr} setSelectedCat={setSelectedCat} selectedCat={selectedCat} filteredData={filteredData} setFilteredData={setFilteredData} inputValue={inputValue} setInputValue={setInputValue} setErrmsg={setErrmsg} setSearchedValue={setSearchedValue} searchedValue={searchedValue} />
         <Sliders />
         <Apicall filteredData={filteredData} inputValue={inputValue} errmsg={errmsg} />
+        <Footer />
       </div>
     </>
   );
-}
+} 
 
 export default App;
